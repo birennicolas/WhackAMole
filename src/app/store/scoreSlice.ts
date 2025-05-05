2d21edc69be2dc35086fc243e013bfd4509d65f3
Nico <birennicolas@gmail.com>
2025-05-05 16:27:06 +0200

    Add reset score to slice

--- src/app/store/scoreSlice.ts
+++ src/app/store/scoreSlice.ts
@@ -8,8 +8,9 @@ const scoreSlice = createSlice({
   reducers: {
     increment: (state) => { state.value += 1; },
     decrement: (state) => { state.value -= 1; },
+    reset: (state) => { state.value = 0; },
   },
 });
 
-export const { increment, decrement } = scoreSlice.actions;
+export const { increment, decrement, reset } = scoreSlice.actions;
 export default scoreSlice.reducer;
