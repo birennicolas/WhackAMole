d803e66e038845f3d9b3aebe48b37071999b5970
Nico <birennicolas@gmail.com>
2025-05-05 23:11:20 +0200

    Switch favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,27 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Whack A Mole",
   description: "The classic game of whacking moles",
+  icons: {
+    icon: [
+      {
+        url: '/favicon/icon.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+      {
+        url: '/favicon/icon.png',
+        sizes: '16x16',
+        type: 'image/png',
+      }
+    ],
+    apple: [
+      {
+        url: '/favicon/icon.png',
+        sizes: '180x180',
+        type: 'image/png',
+      }
+    ],
+  },
 };
 
 export default function RootLayout({
