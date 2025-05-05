50105c136fdf2d4751f57274b3441947b00a1e4d
Nico <birennicolas@gmail.com>
2025-05-05 11:58:03 +0200

    Main page improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { useDispatch } from 'react-redux';
 import { setName } from './store/nameSlice';
 import { useRouter } from 'next/navigation';
 import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -21,7 +22,30 @@ export default function Home() {
   };
 
   return (
-    <Stack width="100vw" height="100vh" justifyContent="center" alignItems="center">
+    <Stack width="100vw" height="100vh" justifyContent="center" alignItems="center" gap={4} sx={{
+      backgroundImage: "url(/images/WAM_bg.jpg)",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat"
+    }}>
+        <Box
+          sx={{
+            color: '#FF0000',
+            fontSize: {
+              xs: '0.7rem',
+              sm: '1.2rem',
+              md: '1.5rem',
+              lg: '1.75rem'
+            },
+            fontWeight: 'bold',
+            textShadow: '2px 2px 4px rgba(247, 241, 241, 0.3)',
+            fontFamily: '"Press Start 2P", cursive',
+            margin: 0,
+            marginBottom: 4
+          }}
+        >
+          Whack a Mole
+        </Box>
         <form onSubmit={handleSubmit} className={styles.form}>
           <input
             type="text"
